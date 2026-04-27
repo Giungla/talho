4f34b80c9e5d0c1908cdb72436ae5f2a30b72670
Wanderson Silva <wandersonsilva768@gmail.com>
2026-04-27 20:03:31 -0300

    feat: melhorias no PageView + configuração de correspondência manual avançada

--- src/page-view.ts
+++ src/page-view.ts
@@ -1,67 +1,18 @@
 
 import {
-  type ResponsePattern,
-  type FunctionErrorPattern,
-  type ResponsePatternCallback,
-  type FunctionSucceededPattern,
-} from '../global'
+  pageViewTracking,
+} from '../utils/tracking'
 
-import {
-  XANO_BASE_URL,
-} from '../utils/consts'
-
-import {
-  postErrorResponse,
-  postSuccessResponse,
-  buildRequestOptions,
-} from '../utils/requestResponse'
-
-import {
-  getMetaTrackingCookies,
-} from '../utils/adTracking'
-
-import {
-  type PageViewResponse,
-} from '../types/pageView'
-
-import {
-  getEndpointType,
-} from '../types/cart'
-
-async function pageView <T extends PageViewResponse> (): Promise<ResponsePattern<T>> {
-  const defaultErrorMessage = 'Não foi possível registrar o evento'
-
-  try {
-    const response = await fetch(`${XANO_BASE_URL}/api:uMv7xbDN/event/page_view/${getEndpointType()}`, {
-      ...buildRequestOptions([
-        ...getMetaTrackingCookies(),
-      ]),
-      keepalive: true,
-      priority: 'high',
-    })
-
-    if (!response.ok) {
-      const error = await response.json()
-
-      return postErrorResponse.call<
-        Response, [string], FunctionErrorPattern
-      >(response, error)
-    }
-
-    const data: T = await response.json()
-
-    return postSuccessResponse.call<
-      Response, [T, ResponsePatternCallback?], FunctionSucceededPattern<T>
-    >(response, data)
-  } catch (e) {
-    return postErrorResponse(defaultErrorMessage)
-  }
-}
-
-pageView().then(response => {
+pageViewTracking().then(response => {
   if (!response.succeeded) return
 
+  const {
+    meta,
+  } = response.data
+
+  fbq?.('init' , meta.app_id, meta.customer_data ?? {})
+
   fbq?.('track', 'PageView', {}, {
-    eventID: response.data.meta_event_id,
+    eventID: meta.event_id,
   })
 })
